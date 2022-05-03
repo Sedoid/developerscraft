d094d384eb746b07e5f02d7f0c0fb4bdcaa98b5f
sedoid <karlsedoide@gmail.com>
2022-05-03 14:48:06 +0100

    update

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -42,7 +42,7 @@ const postDetails = ({post}) => {
                     </Script>
                 </Head>
       
-            <div className="container mx-auto px-lg-10 mb-8">
+            <div className="container mx-auto px-lg-10 px-5 py-5 mb-8">
                 <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
                     <div className="col-span-1 lg:col-span-8">
                         <PostDetail post={post} />
